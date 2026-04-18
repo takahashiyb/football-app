fe5b3cea86876be9c0ab468aa3a66643603de058
takahashiyb <takahashiyb89@gmail.com>
2026-04-19 07:30:59 +0800

    Refactor to have a more modular scss

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use '/src/assets/styles/variables.scss' as v;
+        @use '/src/assets/styles/functions.scss' as f;
+        @use '/src/assets/styles/resets.scss' as reset;
+        `,
+      },
     },
   },
 })
